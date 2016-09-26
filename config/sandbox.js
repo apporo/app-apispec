bbcc0b00a39bbf4fd7a8bd5a3a2c38ea7139f72e
pnhung177 <pnhung177@acegik.net>
2016-09-26 17:29:17 +0700

    Implements the login form to get JWTToken after authentication

--- config/sandbox.js
+++ config/sandbox.js
@@ -1,8 +1,33 @@
 module.exports = {
   plugins: {
     appApispec: {
+      apiTokenName: 'x-access-token',
+      authenticationUrl: '/tokenify/auth',
       ui: {
         title: 'apispec'
+      },
+      loginForm: {
+        field: {
+          realm: {
+            enabled: false,
+            id: 'realm',
+            label: 'Realm',
+            pattern: '^[a-zA-Z][a-zA-Z0-9-_\.]{3,20}$',
+            placeholder: 'Realm must contain only alphabets or digits (3 - 20 characters)'
+          },
+          username: {
+            id: 'username',
+            label: 'Username',
+            pattern: '^[a-zA-Z][a-zA-Z0-9-_\.]{3,20}$',
+            placeholder: 'Username must be between 3 and 20 characters'
+          },
+          password: {
+            id: 'password',
+            label: 'Password',
+            pattern: '(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$',
+            placeholder: 'Password must contain 1 uppercase, lowercase and number'
+          }
+        }
       }
     }
   }
